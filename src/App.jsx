ecd0ed059353de77571c44d90298087cdb4e929d
Godswill Okoi <119078076+smartmoses@users.noreply.github.com>
2023-10-31 08:24:19 +0100

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ const App = () => {
           placeholder="Search for movies"
         />
         <img
+        
           src={SearchIcon}
           alt="search"
           onClick={() => searchMovies(searchTerm)}
@@ -52,6 +53,7 @@ const App = () => {
     </div>
   );
 };
+
 
 export default App;
 
