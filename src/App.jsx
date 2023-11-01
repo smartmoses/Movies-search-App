e35e08874025c0e54ad4327e5a125dcab5db3547
Godswill Okoi <119078076+smartmoses@users.noreply.github.com>
2023-11-01 13:25:55 +0100

    changed link to https

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./assets/search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=85ccab0a";
+const API_URL = "https://www.omdbapi.com?apikey=85ccab0a";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
